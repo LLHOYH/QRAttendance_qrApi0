8e6e7fffe19907a68a0d44fc8fcd5a046094b0db
LLHOYH <llhoyh01@gmail.com>
2020-05-28 15:12:05 +0800

    changes

--- server.js
+++ server.js
@@ -687,7 +687,7 @@ app.get('/LocationSettings', cors(corsOptions), function (request, response) {
 app.get('/ChangeDeviceSettings', cors(corsOptions), function (request, response) {
     response.send({
         "Success":true,
-        "Setting_Results":{ChangeDeviceEnabled: true, NumberOfTimes:0}
+        "Setting_Results":{ChangeDeviceEnabled: true, NumberOfTimes:3}
     });
 });
 
